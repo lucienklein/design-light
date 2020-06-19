ee72a41b6882a6036afe68b284c548c86c9346c7
Lucien Klein <lucien_klein@outlook.com>
2020-06-19 17:12:05 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row align="center">
+      <v-col>
+        <v-img src="@/assets/logo.png" height="25vh" contain></v-img>
+      </v-col>
+    </v-row>
+    <hr />
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+hr {
+  color: #666666;
+  margin-top: 2rem;
+  width: 80vh;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="auto" align="center">
+        <v-img src="@/assets/logo.png" class="img" contain></v-img>
+        <hr />
+        <h2>Working in progress</h2>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+hr {
+  color: #666666;
+  margin-top: 2rem;
+  width: calc(40vw + 10rem);
+}
+
+.img {
+  width: calc(30vw + 10rem);
+}
+</style>
